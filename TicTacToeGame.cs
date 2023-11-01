80b329515ab086416f5b5a9b93eafa7a9a79b3a4
henrik-m <holy-clive@gmx.de>
2023-11-01 15:43:22 +0100

    Introduce hard difficulty where the computer does not play random, but makes optimal moves

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -1,4 +1,8 @@
 using Godot;
+using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
 
 public partial class TicTacToeGame : Node2D
 {
@@ -9,8 +13,17 @@ public partial class TicTacToeGame : Node2D
 		O
 	}
 
+	enum Difficulty
+	{
+		Easy,
+		Hard
+	}
+
 	private CellState[,] gameBoard = new CellState[3, 3];
 	private CellState currentPlayer = CellState.X;
+	private CellState humanPlayer = CellState.X;
+	private CellState computerPlayer = CellState.O;
+	private Difficulty currentDifficulty = Difficulty.Hard;
 
 	public override void _Ready()
 	{
@@ -52,16 +65,32 @@ public partial class TicTacToeGame : Node2D
 			}
 		}
 		currentPlayer = CellState.X;
+
+		if (currentPlayer == computerPlayer)
+		{
+			MakeComputerMove();
+		}
 	}
 
 	private void OnCellPressed(int x, int y)
 	{
-		if (gameBoard[x, y] == CellState.Empty)
+		if (gameBoard[x, y] == CellState.Empty && currentPlayer == humanPlayer)
 		{
 			gameBoard[x, y] = currentPlayer;
 			UpdateCell(x, y);
-			CheckForWinner();
-			currentPlayer = (currentPlayer == CellState.X) ? CellState.O : CellState.X;
+			if (CheckForWinner(gameBoard, out var winner))
+			{
+				ShowGameOverDialog(winner);
+			}
+			else
+			{
+				SwitchPlayer();
+
+				if (currentPlayer == computerPlayer)
+				{
+					MakeComputerMove();
+				}
+			}
 		}
 	}
 
@@ -86,68 +115,69 @@ public partial class TicTacToeGame : Node2D
 		};
 	}
 
-	private void CheckForWinner()
+	private static bool CheckForWinner(CellState[,] board, out CellState winner)
 	{
-		var winner = CheckForWinnerInRows();
+		winner = CheckForWinnerInRows(board);
 		if (winner == CellState.Empty)
 		{
-			winner = CheckForWinnerInColumns();
+			winner = CheckForWinnerInColumns(board);
 		}
 		if (winner == CellState.Empty)
 		{
-			winner = CheckForWinnerInDiagonals();
+			winner = CheckForWinnerInDiagonals(board);
 		}
-		if (winner != CellState.Empty || IsBoardFull())
+		if (winner != CellState.Empty || IsBoardFull(board))
 		{
-			ShowGameOverDialog(winner);
+			return true;
 		}
+		return false;
 	}
 
-	private bool IsBoardFull()
+	private static bool IsBoardFull(CellState[,] board)
 	{
-    foreach (var cell in gameBoard)
+		foreach (var cell in board)
 		{
-      if (cell == CellState.Empty)
+			if (cell == CellState.Empty)
 			{
-        return false;
-      }
-    }
-    return true;
-  }
+				return false;
+			}
+		}
+		return true;
+	}
 
-	private CellState CheckForWinnerInRows()
+	private static CellState CheckForWinnerInRows(CellState[,] board)
 	{
 		for (int y = 0; y < 3; y++)
 		{
-			if (gameBoard[0, y] != CellState.Empty && gameBoard[0, y] == gameBoard[1, y] && gameBoard[1, y] == gameBoard[2, y])
+			if (board[0, y] != CellState.Empty && board[0, y] == board[1, y] && board[1, y] == board[2, y])
 			{
-				return gameBoard[0, y];
+				return board[0, y];
 			}
 		}
 		return CellState.Empty;
 	}
 
-	private CellState CheckForWinnerInColumns()
+	private static CellState CheckForWinnerInColumns(CellState[,] board)
 	{
 		for (int x = 0; x < 3; x++)
 		{
-			if (gameBoard[x, 0] != CellState.Empty && gameBoard[x, 0] == gameBoard[x, 1] && gameBoard[x, 1] == gameBoard[x, 2])
+			if (board[x, 0] != CellState.Empty && board[x, 0] == board[x, 1] && board[x, 1] == board[x, 2])
 			{
-				return gameBoard[x, 0];
+				return board[x, 0];
 			}
 		}
 		return CellState.Empty;
 	}
 
-	private CellState CheckForWinnerInDiagonals()
+	private static CellState CheckForWinnerInDiagonals(CellState[,] board)
 	{
-		if (gameBoard[0, 0] != CellState.Empty && gameBoard[0, 0] == gameBoard[1, 1] && gameBoard[1, 1] == gameBoard[2, 2])
+		if (board[0, 0] != CellState.Empty && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
 		{
-			return gameBoard[0, 0];
+			return board[0, 0];
 		}
-		if (gameBoard[2, 0] != CellState.Empty && gameBoard[2, 0] == gameBoard[1, 1] && gameBoard[1, 1] == gameBoard[0, 2])
+		if (board[2, 0] != CellState.Empty && board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2])
 		{
-			return gameBoard[2, 0];
+			return board[2, 0];
 		}
 		return CellState.Empty;
 	}
@@ -158,13 +188,178 @@ public partial class TicTacToeGame : Node2D
 
 		if (winner == CellState.Empty)
 		{
-      winnerDialog.DialogText = "It's a tie!";
-    }
-    else
+			winnerDialog.DialogText = "It's a tie!";
+		}
+		else
 		{
-      winnerDialog.DialogText = $"Player {winner} wins!";
-    }
+			winnerDialog.DialogText = $"Player {winner} wins!";
+		}
 
 		winnerDialog.PopupCentered();
+	}
+
+	private void SwitchPlayer()
+	{
+		currentPlayer = (currentPlayer == CellState.X) ? CellState.O : CellState.X;
+	}
+
+	private void MakeComputerMove()
+	{
+		// Choose a random move from the available moves
+		if (!IsBoardFull(gameBoard))
+		{
+			var move = currentDifficulty switch
+			{
+				Difficulty.Easy => GetRandomMove(gameBoard),
+				Difficulty.Hard => GetOptimalMove(gameBoard, humanPlayer, computerPlayer),
+				_ => throw new NotSupportedException($"Difficulty {currentDifficulty} is not supported."),
+			};
+
+			int x = (int)move.X;
+			int y = (int)move.Y;
+
+			// Make the move
+			gameBoard[x, y] = currentPlayer;
+			UpdateCell(x, y);
+			if (CheckForWinner(gameBoard, out var winner))
+			{
+				ShowGameOverDialog(winner);
+			}
+			else
+			{
+				SwitchPlayer();
+			}
+		}
+	}
+
+	private static CellState[,] CopyBoard(CellState[,] board)
+	{
+		var tempBoard = new CellState[3, 3];
+		Array.Copy(board, tempBoard, board.Length);
+		return tempBoard;
+	}
+
+	private static void PrintBoard(CellState[,] board)
+	{
+		var boardString = new StringBuilder();
+
+		for (int x = 0; x < 3; x++)
+		{
+			for (int y = 0; y < 3; y++)
+			{
+				var symbol = board[x, y] switch
+				{
+					CellState.X => "X",
+					CellState.O => "O",
+					_ => "-",
+				};
+				boardString.Append(symbol);
+
+				if (y == 2)
+				{
+					boardString.AppendLine();
+				}
+			}
+		}
+
+		GD.Print(boardString.ToString());
+	}
+
+	private static Vector2 GetOptimalMove(CellState[,] board, CellState humanPlayer, CellState computerPlayer)
+	{
+		Vector2 bestMove = new Vector2(-1, -1);
+		int bestScore = int.MinValue;
+
+		var tempBoard = CopyBoard(board);
+
+		for (int x = 0; x < 3; x++)
+		{
+			for (int y = 0; y < 3; y++)
+			{
+				if (tempBoard[x, y] == CellState.Empty)
+				{
+					tempBoard[x, y] = computerPlayer;
+					int moveScore = Minimax(tempBoard, 0, false, humanPlayer, computerPlayer);
+
+					GD.Print($"Move: {x}, {y}, Score: {moveScore}");
+					PrintBoard(tempBoard);
+
+					tempBoard[x, y] = CellState.Empty;
+
+					if (moveScore > bestScore)
+					{
+						bestScore = moveScore;
+						bestMove = new Vector2(x, y);
+					}
+				}
+			}
+		}
+
+		return bestMove;
+	}
+
+	private static Vector2 GetRandomMove(CellState[,] board)
+	{
+		// Get a list of available moves
+		var availableMoves = new List<Vector2>();
+		for (int x = 0; x < 3; x++)
+		{
+			for (int y = 0; y < 3; y++)
+			{
+				if (board[x, y] == CellState.Empty)
+				{
+					availableMoves.Add(new Vector2(x, y));
+				}
+			}
+		}
+
+		return availableMoves[GD.RandRange(0, availableMoves.Count - 1)];
+	}
+
+	private static int Minimax(CellState[,] board, int depth, bool isMaximizing, CellState player, CellState computer)
+	{
+		if (CheckForWinner(board, out var winner))
+		{
+			if (winner == player) return depth - 10;
+			if (winner == computer) return 10 - depth;
+			return 0;
+		}
+
+		if (isMaximizing)
+		{
+			int maxEval = int.MinValue;
+			for (int x = 0; x < 3; x++)
+			{
+				for (int y = 0; y < 3; y++)
+				{
+					if (board[x, y] == CellState.Empty)
+					{
+						board[x, y] = computer; // Maximizing for computer
+						int eval = Minimax(board, depth + 1, false, player, computer);
+						board[x, y] = CellState.Empty;
+						maxEval = Math.Max(maxEval, eval);
+					}
+				}
+			}
+			return maxEval;
+		}
+		else
+		{
+			int minEval = int.MaxValue;
+			for (int x = 0; x < 3; x++)
+			{
+				for (int y = 0; y < 3; y++)
+				{
+					if (board[x, y] == CellState.Empty)
+					{
+						board[x, y] = player; // Minimizing for player
+						int eval = Minimax(board, depth + 1, true, player, computer);
+						board[x, y] = CellState.Empty;
+						minEval = Math.Min(minEval, eval);
+					}
+				}
+			}
+			return minEval;
+		}
 	}
 }
